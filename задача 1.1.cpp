498f1e26ae3ff4bad336ffe9c203e695ded3ec45
ruslanITstep <75069415+ruslanITstep@users.noreply.github.com>
2020-11-26 16:05:36 +0600

    Add files via upload

---
+++ задача 1.1.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "идти на вахту" << endl;
+	cout << "назвать номер кабинета: " << endl;
+	int n = 0;
+	cin >> n;
+	cout << endl;
+	cout << "взять ключ" << endl;
+	cout << "вернуться обратно" << endl;
+}
+
