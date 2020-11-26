f34e579cc6753006a4e9bd9467956b5c1f3b86d8
ruslanITstep <75069415+ruslanITstep@users.noreply.github.com>
2020-11-26 17:28:00 +0600

    Add files via upload

---
+++ конторольная.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Enter your name: ";
+    std::string myName;
+    std::cin >> myName;
+
+    std::cout << "Enter your full surname: ";
+    std::string mySurname;
+    std::cin >> mySurname;
+
+    std::cout << "Enter your age: ";
+    std::string myAge;
+    std::cin >> myAge;
+
+    std::cout << "Your name is " << myName << " Your surname is   " << mySurname << " and your age is " << myAge;
+    return 0;
+}
