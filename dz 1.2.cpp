d2369bdef37d035311bc912a5d9cdd08c10d585b
ruslanITstep <75069415+ruslanITstep@users.noreply.github.com>
2020-11-30 00:08:52 +0600

    Add files via upload

---
+++ dz 1.2.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	cout << "выдается ключ" << endl;
+	int n = rand() % 100;
+	cout << n << endl;
+	cout << "введите ключ" << endl;
+	int x = 0;
+	cin >> x;
+
+}
---
+++ dz 1.3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Enter your name: ";
+    std::string myName;
+    std::cin >> myName;
+
+    std::cout << "ok  " << myName << " exterminate all goblins from the cave  ";
+    return 0;
+}
