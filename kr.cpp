f0d47ef60d3f020b763356c630b9f37f41fc2ed0
ruslanITstep <75069415+ruslanITstep@users.noreply.github.com>
2020-12-07 16:19:29 +0600

    Add files via upload

---
+++ kr.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int n = 77; // vibrat' chislo
+	if (n > 80)
+		cout << "trying answering" << endl;
+	else if (n < 50)
+		cout << "i can not understand" << endl;
+	else if (50 < n < 80)
+		cout << "i am true?" << endl;
+	return 0;
+
+}
